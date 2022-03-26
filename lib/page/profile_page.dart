69f8d6fd6deb88c088097347f81eb7d307f02561
quanphung0106-sudo <phungdinhquan16@gmail.com>
2022-03-26 20:33:11 +0700

    updated user avatar image

--- lib/page/profile_page.dart
+++ lib/page/profile_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:profile_1/model/user.dart';
+import 'package:profile_1/utils/user_preferences.dart';
 import 'package:profile_1/widget/appbar_widget.dart';
+import 'package:profile_1/widget/button_widget.dart';
+import 'package:profile_1/widget/profile_widget.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -11,14 +15,40 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
+    final user = UserPreferences.myUser;
+
     return Scaffold(
       appBar: buildAppBar(context),
       body: ListView(
         physics: BouncingScrollPhysics(),
         children: [
-          
+          ProfileWidget(
+            imagePath: user.imagePath,
+            onClicked: () async {},
+          ),
+          const SizedBox(height: 24),
+          buildName(user),
+          const SizedBox(height: 24),
+          Center(child: buildUpgradeButton()),
         ],
       ),
     );
   }
+
+  Widget buildName(User user) => Column(
+        children: [
+          Text(
+            user.name,
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+          ),
+          const SizedBox(height: 6),
+          Text(
+            user.email,
+            style: TextStyle(color: Colors.grey),
+          ),
+        ],
+      );
+
+  Widget buildUpgradeButton() =>
+      ButtonWidget(text: 'Support me!', onClicked: () {});
 }
