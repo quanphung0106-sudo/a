d468d322334b8f7d70ef4e2f471044a6108389d0
quanphung0106-sudo <phungdinhquan16@gmail.com>
2022-03-26 18:49:34 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:profile_1/page/profile_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static final String title = 'User Profile';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.blue.shade300,
+      ),
+      title: title,
+      home: ProfilePage(),
+    );
+  }
+}
